994daf0680c29e3d808c493e06d6b521c00bd136
Kobe <majorkobe@gmail.com>
2022-12-06 17:14:42 -0500

    python data mangling

--- uber_test.py
+++ uber_test.py
@@ -1,12 +1,12 @@
 import json
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-#import matplotlib.pyplot as plt
 import seaborn as sns
+import random
 
-infile = "uber_data/uber.csv"
-outfile_filtered = "uber_data/uber_filtered.csv"
-outfile_condensed = "uber_data/uber_condensed.csv"
+infile = "TSP/uber_data/uber.csv"
+outfile_filtered = "TSP/uber_data/uber_filtered.csv"
+outfile_condensed = "TSP/uber_condensed.csv"
 trips_raw = pd.read_csv(infile)
 
 #Time information is presented in two columns (key and pickup_datetime).
@@ -27,6 +27,7 @@ trips_raw['distance'] = np.sqrt((lat1 - lat2)**2 + (lon1 - lon2)**2)
 trips_raw = trips_raw.round({'pickup_longitude':1, 'pickup_latitude': 1, 
                             'dropoff_latitude':1,'dropoff_longitude':1})
 
+#remove outliers in data
 fare_low = trips_raw['fare_amount'].quantile(0.05)
 fare_hi = trips_raw['fare_amount'].quantile(0.95)
 dist_low = trips_raw['distance'].quantile(0.05)
@@ -37,26 +38,24 @@ trip_outliers = trips_raw[(trips_raw["distance"] < dist_hi) &
     & (trips_raw['fare_amount'] > fare_low)]
 
 trips_filtered = trip_outliers
-
 trips_filtered = trips_filtered.groupby(['pickup_longitude','pickup_latitude','dropoff_latitude','dropoff_longitude']).mean()
 trips_filtered['edge'] = range(1, len(trips_filtered) + 1)
-
 trips_condensed = trips_filtered[['edge','distance']]
-
 trips_condensed = trips_condensed.reset_index()
 
 
-# trips_condensed['pickup_id'] = trips_condensed.groupby(['pickup_longitude','pickup_latitude']).ngroup()
-# trips_condensed['dropoff_id'] = trips_condensed.groupby(['dropoff_longitude','dropoff_latitude']).ngroup()
 
+#convert columns to lists 
 pickup_longitude = trips_condensed['pickup_longitude'].tolist()
 pickup_latitude = trips_condensed['pickup_latitude'].tolist()
-
 dropoff_longitude = trips_condensed['dropoff_longitude'].tolist()
 dropoff_latitude = trips_condensed['dropoff_latitude'].tolist()
 
 node_dict = {}
 
+
+
+#give unique ids to every node, count should be the total amount of unique nodes at the end of the loops
 count = 0 
 for i in range(len(pickup_longitude)):
     index = node_dict.get((pickup_longitude[i],pickup_latitude[i]),-1)
@@ -70,17 +69,34 @@ for i in range(len(dropoff_longitude)):
         node_dict[(dropoff_longitude[i],dropoff_latitude[i])] = count
         count+=1
 
-# for row in trips_condensed:
-#     row['pickup_node'] = node_dict[(row['pickup_longitude'],row['pickup_latitude'])] 
-#     row['dropoff_node'] = node_dict[(row['dropoff_longitude'],row['dropoff_latitude'])]
 
+
+#apply dictionary get function for every row to apply node enumeration
 trips_condensed['dropoff_node'] = trips_condensed.apply(lambda row: node_dict.get((row['dropoff_longitude'],row['dropoff_latitude']),-1),axis = 1)
 trips_condensed['pickup_node'] = trips_condensed.apply(lambda row: node_dict.get((row['pickup_longitude'],row['pickup_latitude']),-1),axis = 1)
 
+nodes = 64
+for drop_node in range(nodes):
+    for pickup_node in range(nodes):
+        if not (((trips_condensed['dropoff_node']==drop_node) & (trips_condensed['pickup_node']==pickup_node)).any()):
+            print(f'not included dropnode: {drop_node} pickupnode: {pickup_node}')
+            filler_distance = random.uniform(0.006,0.09)
+            new_row = {'dropoff_node': drop_node, 'pickup_node':pickup_node,'distance':filler_distance}
+
+            print(trips_condensed.columns)
+
+            df_new_row = pd.DataFrame({ 'pickup_longitude': [0], 'pickup_latitude': [0],'dropoff_latitude':[0],'dropoff_longitude':[0],'edge':[0],'distance':[filler_distance],'dropoff_node':[drop_node],'pickup_node':[pickup_node]})
+            trips_condensed = pd.concat([trips_condensed, df_new_row])               
+           
+
+
+
+
+
+
+
 
 print(node_dict)
-# for row in trips_condensed:
-#     print(int(row['pickup_latitude']))
 
 trips_condensed = trips_condensed[['dropoff_node','pickup_node','distance','dropoff_longitude','dropoff_latitude','pickup_longitude','pickup_latitude']]
 print(trips_condensed)
